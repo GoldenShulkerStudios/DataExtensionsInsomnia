9f5420d0875ca00eacb4b45120970077a5192031
GoldenShulkerStudios <goldenshulkerstudios@gmail.com>
2024-03-07 12:47:52 -0500

    modificaciones muchas modificaciones

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", function() {
     var tabContents = document.querySelectorAll('.tab-content');
     var subButtons = document.querySelectorAll('.sub-buttons a');
 
-    // Función para cambiar los contenidos al hacer clic en los botones
     buttons.forEach(function(button) {
         button.addEventListener('click', function(e) {
             e.preventDefault();
@@ -12,20 +11,21 @@ document.addEventListener("DOMContentLoaded", function() {
                 content.style.display = 'none';
             });
             document.getElementById(target).style.display = 'block';
+            
+            var allSubButton = document.querySelector('[data-content="all"]');
+            allSubButton.click();
         });
     });
 
-    // Función para filtrar archivos según las etiquetas al hacer clic en los elementos de la lista de sub botones
     subButtons.forEach(function(subButton) {
         subButton.addEventListener('click', function(e) {
             e.preventDefault();
             var targetTag = subButton.getAttribute('data-content');
             var files = document.querySelectorAll('.file');
 
-            // Mostrar solo los archivos que tienen la etiqueta correspondiente
             files.forEach(function(file) {
-                var tags = file.getAttribute('data-tags').split(' ');
-                if (tags.includes(targetTag) || targetTag === 'all') {
+                var tags = file.getAttribute('data-tags');
+                if (tags === targetTag || targetTag === 'all') {
                     file.style.display = 'block';
                 } else {
                     file.style.display = 'none';
